b1ed7cfab58688e8be542aa9ff0d634020c96c7b
Myke Onyedum <mykeonyedum@Mykes-MacBook-Air.local>
2021-12-17 13:42:12 -0500

    base file

---
+++ src/main/java/timestoppers/llc/ourglass/demo/OurglassDemoApplication.java
@@ -0,0 +1,13 @@
+package timestoppers.llc.ourglass.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurglassDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurglassDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/timestoppers/llc/ourglass/demo/OurglassDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package timestoppers.llc.ourglass.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurglassDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
